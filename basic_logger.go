55a25f584fbb1e3c04e07a895e70110c02ac44bd
Jonathan Levison <jonathanl@orbs.com>
2018-08-18 09:47:06 +0300

    rename log package

---
+++ basic_logger.go
@@ -0,0 +1,275 @@
+package log
+
+import (
+	"fmt"
+	"github.com/orbs-network/orbs-network-go/crypto/base58"
+	"github.com/orbs-network/orbs-spec/types/go/primitives"
+	"os"
+	"reflect"
+	"runtime"
+	"strings"
+	"time"
+)
+
+type BasicLogger interface {
+	Log(level string, message string, params ...*Field)
+	Info(message string, params ...*Field)
+	Error(message string, params ...*Field)
+	Metric(name string, params ...*Field)
+	For(params ...*Field) BasicLogger
+	Meter(name string, params ...*Field) BasicMeter
+	Prefixes() []*Field
+	WithOutput(writer ...Output) BasicLogger
+}
+
+type basicLogger struct {
+	outputs               []Output
+	prefixes              []*Field
+	nestingLevel          int
+	sourceRootPrefixIndex int
+}
+
+type FieldType uint8
+
+const (
+	NoType = iota
+	ErrorType
+	NodeType
+	ServiceType
+	StringType
+	IntType
+	UintType
+	BytesType
+	FloatType
+	FunctionType
+	SourceType
+	StringArrayType
+)
+
+type Field struct {
+	Key  string
+	Type FieldType
+
+	String      string
+	StringArray []string
+	Int         int64
+	Uint        uint64
+	Bytes       []byte
+	Float       float64
+
+	Error error
+}
+
+func Node(value string) *Field {
+	return &Field{Key: "node", String: value, Type: NodeType}
+}
+
+func Service(value string) *Field {
+	return &Field{Key: "service", String: value, Type: ServiceType}
+}
+
+func Function(value string) *Field {
+	return &Field{Key: "function", String: value, Type: FunctionType}
+}
+
+func Source(value string) *Field {
+	return &Field{Key: "source", String: value, Type: SourceType}
+}
+
+func String(key string, value string) *Field {
+	return &Field{Key: key, String: value, Type: StringType}
+}
+
+func Stringable(key string, value fmt.Stringer) *Field {
+	return &Field{Key: key, String: value.String(), Type: StringType}
+}
+
+func StringableSlice(key string, values interface{}) *Field {
+	var strings []string
+	switch reflect.TypeOf(values).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(values)
+
+		strings = make([]string, 0, s.Len())
+
+		for i := 0; i < s.Len(); i++ {
+			if stringer, ok := s.Index(i).Interface().(fmt.Stringer); ok {
+				strings = append(strings, stringer.String())
+			}
+		}
+	}
+
+	return &Field{Key: key, StringArray: strings, Type: StringArrayType}
+}
+
+func Int(key string, value int) *Field {
+	return &Field{Key: key, Int: int64(value), Type: IntType}
+}
+
+func Int32(key string, value int32) *Field {
+	return &Field{Key: key, Int: int64(value), Type: IntType}
+}
+
+func Int64(key string, value int64) *Field {
+	return &Field{Key: key, Int: int64(value), Type: IntType}
+}
+
+func Bytes(key string, value []byte) *Field {
+	return &Field{Key: key, Bytes: value, Type: BytesType}
+}
+
+func Uint(key string, value uint) *Field {
+	return &Field{Key: key, Uint: uint64(value), Type: UintType}
+}
+
+func Uint32(key string, value uint32) *Field {
+	return &Field{Key: key, Uint: uint64(value), Type: UintType}
+}
+
+func Uint64(key string, value uint64) *Field {
+	return &Field{Key: key, Uint: value, Type: UintType}
+}
+
+func Float32(key string, value float32) *Field {
+	return &Field{Key: key, Float: float64(value), Type: FloatType}
+}
+
+func Float64(key string, value float64) *Field {
+	return &Field{Key: key, Float: value, Type: FloatType}
+}
+
+func Error(value error) *Field {
+	return &Field{Key: "error", Error: value, Type: ErrorType}
+}
+
+func BlockHeight(value primitives.BlockHeight) *Field {
+	return &Field{Key: "block-height", String: value.String(), Type: StringType}
+}
+
+func GetLogger(params ...*Field) BasicLogger {
+	logger := &basicLogger{
+		prefixes:     params,
+		nestingLevel: 4,
+		outputs:      []Output{&basicOutput{output: os.Stdout, formatter: NewJsonFormatter()}},
+	}
+
+	fpcs := make([]uintptr, 1)
+	n := runtime.Callers(logger.nestingLevel, fpcs)
+	if n != 0 {
+		fun := runtime.FuncForPC(fpcs[0] - 1)
+		if fun != nil {
+			file, _ := fun.FileLine(fpcs[0] - 1)
+			if l := strings.Index(file, "orbs-network-go/"); l > -1 {
+				logger.sourceRootPrefixIndex = l + len("orbs-network-go/")
+			}
+		}
+	}
+
+	return logger
+}
+
+func (b *basicLogger) getCaller(level int) (function string, source string) {
+	fpcs := make([]uintptr, 1)
+
+	// skip levels to get to the caller of logger function
+	n := runtime.Callers(level, fpcs)
+	if n == 0 {
+		return "n/a", "n/a"
+	}
+
+	fun := runtime.FuncForPC(fpcs[0] - 1)
+	if fun == nil {
+		return "n/a", "n/a"
+	}
+
+	file, line := fun.FileLine(fpcs[0] - 1)
+	fName := fun.Name()
+	lastSlashOfName := strings.LastIndex(fName, "/")
+	if lastSlashOfName > 0 {
+		fName = fName[lastSlashOfName+1:]
+	}
+	return fName, fmt.Sprintf("%s:%d", file[b.sourceRootPrefixIndex:], line)
+}
+
+func (b *basicLogger) Prefixes() []*Field {
+	return b.prefixes
+}
+
+func (b *basicLogger) For(params ...*Field) BasicLogger {
+	prefixes := append(b.prefixes, params...)
+	return &basicLogger{prefixes: prefixes, nestingLevel: b.nestingLevel, outputs: b.outputs}
+}
+
+func (b *basicLogger) Metric(metric string, params ...*Field) {
+	metricParams := append(params, String("metric", metric))
+	b.Log("metric", "Metric recorded", metricParams...)
+}
+
+func (f *Field) Value() interface{} {
+	switch f.Type {
+	case NodeType:
+		return f.String
+	case ServiceType:
+		return f.String
+	case FunctionType:
+		return f.String
+	case SourceType:
+		return f.String
+	case StringType:
+		return f.String
+	case IntType:
+		return f.Int
+	case UintType:
+		return f.Uint
+	case BytesType:
+		return base58.Encode(f.Bytes)
+	case FloatType:
+		return f.Float
+	case ErrorType:
+		if f.Error != nil {
+			return f.Error.Error()
+		} else {
+			return "<nil>"
+		}
+		return f.Error.Error()
+	case StringArrayType:
+		return f.StringArray
+	}
+
+	return nil
+}
+
+func (b *basicLogger) Log(level string, message string, params ...*Field) {
+	function, source := b.getCaller(b.nestingLevel)
+
+	enrichmentParams := []*Field{
+		Function(function),
+		Source(source),
+	}
+
+	enrichmentParams = append(enrichmentParams, b.prefixes...)
+	enrichmentParams = append(enrichmentParams, params...)
+
+	for _, output := range b.outputs {
+		logLine := output.Formatter().FormatRow(level, message, enrichmentParams...)
+		fmt.Fprintln(output.Output(), logLine)
+	}
+}
+
+func (b *basicLogger) Info(message string, params ...*Field) {
+	b.Log("info", message, params...)
+}
+
+func (b *basicLogger) Error(message string, params ...*Field) {
+	b.Log("error", message, params...)
+}
+
+func (b *basicLogger) Meter(name string, params ...*Field) BasicMeter {
+	meterLogger := &basicLogger{nestingLevel: 5, prefixes: b.prefixes, outputs: b.outputs}
+	return &basicMeter{name: name, start: time.Now().UnixNano(), logger: meterLogger, params: params}
+}
+
+func (b *basicLogger) WithOutput(writers ...Output) BasicLogger {
+	b.outputs = writers
+	return b
+}
---
+++ basic_logger_test.go
@@ -0,0 +1,248 @@
+package log_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	. "github.com/onsi/gomega"
+	"github.com/orbs-network/orbs-network-go/instrumentation/log"
+	"github.com/orbs-network/orbs-network-go/test/builders"
+	"github.com/orbs-network/orbs-spec/types/go/primitives"
+	"github.com/orbs-network/orbs-spec/types/go/protocol"
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+)
+
+const (
+	TransactionFlow     = "TransactionFlow"
+	TransactionAccepted = "Transaction accepted"
+)
+
+func captureStdout(f func(writer io.Writer)) string {
+	r, w, _ := os.Pipe()
+
+	f(w)
+
+	w.Close()
+
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+	return buf.String()
+}
+
+func parseOutput(input string) map[string]interface{} {
+	jsonMap := make(map[string]interface{})
+	_ = json.Unmarshal([]byte(input), &jsonMap)
+	return jsonMap
+}
+
+func TestSimpleLogger(t *testing.T) {
+	RegisterTestingT(t)
+
+	stdout := captureStdout(func(writer io.Writer) {
+		serviceLogger := log.GetLogger(log.Node("node1"), log.Service("public-api")).WithOutput(log.NewOutput(writer))
+		serviceLogger.Info("Service initialized")
+	})
+
+	fmt.Println(stdout)
+	jsonMap := parseOutput(stdout)
+
+	Expect(jsonMap["level"]).To(Equal("info"))
+	Expect(jsonMap["node"]).To(Equal("node1"))
+	Expect(jsonMap["service"]).To(Equal("public-api"))
+	Expect(jsonMap["function"]).To(Equal("log_test.TestSimpleLogger.func1"))
+	Expect(jsonMap["message"]).To(Equal("Service initialized"))
+	Expect(jsonMap["source"]).NotTo(BeEmpty())
+	Expect(jsonMap["timestamp"]).NotTo(BeNil())
+}
+
+func TestNestedLogger(t *testing.T) {
+	RegisterTestingT(t)
+
+	stdout := captureStdout(func(writer io.Writer) {
+		serviceLogger := log.GetLogger(log.Node("node1"), log.Service("public-api")).WithOutput(log.NewOutput(writer))
+		txId := log.String("txId", "1234567")
+		txFlowLogger := serviceLogger.For(log.String("flow", TransactionFlow))
+		txFlowLogger.Info(TransactionAccepted, txId, log.Bytes("payload", []byte{1, 2, 3, 99}))
+	})
+
+	fmt.Println(stdout)
+	jsonMap := parseOutput(stdout)
+
+	Expect(jsonMap["level"]).To(Equal("info"))
+	Expect(jsonMap["node"]).To(Equal("node1"))
+	Expect(jsonMap["service"]).To(Equal("public-api"))
+	Expect(jsonMap["function"]).To(Equal("log_test.TestNestedLogger.func1"))
+	Expect(jsonMap["message"]).To(Equal(TransactionAccepted))
+	Expect(jsonMap["source"]).NotTo(BeEmpty())
+	Expect(jsonMap["timestamp"]).NotTo(BeNil())
+	Expect(jsonMap["txId"]).To(Equal("1234567"))
+	Expect(jsonMap["flow"]).To(Equal(TransactionFlow))
+	Expect(jsonMap["payload"]).To(Equal("MlZmV0E="))
+}
+
+func TestStringableSlice(t *testing.T) {
+	RegisterTestingT(t)
+
+	var receipts []*protocol.TransactionReceipt
+
+	receipts = append(receipts, builders.TransactionReceipt().Build())
+	receipts = append(receipts, builders.TransactionReceipt().Build())
+
+	stdout := captureStdout(func(writer io.Writer) {
+		serviceLogger := log.GetLogger(log.Node("node1"), log.Service("public-api")).WithOutput(log.NewOutput(writer))
+		serviceLogger.Info("StringableSlice test", log.StringableSlice("a-collection", receipts))
+	})
+
+	fmt.Println(stdout)
+	jsonMap := parseOutput(stdout)
+
+	Expect(jsonMap["level"]).To(Equal("info"))
+	Expect(jsonMap["node"]).To(Equal("node1"))
+	Expect(jsonMap["service"]).To(Equal("public-api"))
+	Expect(jsonMap["function"]).To(Equal("log_test.TestStringableSlice.func1"))
+	Expect(jsonMap["message"]).To(Equal("StringableSlice test"))
+	Expect(jsonMap["source"]).NotTo(BeEmpty())
+	Expect(jsonMap["timestamp"]).NotTo(BeNil())
+	Expect(jsonMap["a-collection"]).ToNot(Equal("[]"))
+
+	Expect(jsonMap["a-collection"]).To(Equal([]interface{}{
+		"{Txhash:736f6d652d74782d68617368,ExecutionResult:EXECUTION_RESULT_SUCCESS,OutputArguments:[],}",
+		"{Txhash:736f6d652d74782d68617368,ExecutionResult:EXECUTION_RESULT_SUCCESS,OutputArguments:[],}",
+	}))
+}
+
+func TestStringableSliceCustomFormat(t *testing.T) {
+	RegisterTestingT(t)
+
+	var transactions []*protocol.SignedTransaction
+
+	transactions = append(transactions, builders.TransferTransaction().Build())
+	transactions = append(transactions, builders.TransferTransaction().Build())
+	transactions = append(transactions, builders.TransferTransaction().Build())
+	transactions = append(transactions, builders.TransferTransaction().Build())
+
+	stdout := captureStdout(func(writer io.Writer) {
+		serviceLogger := log.GetLogger(log.Node("node1"), log.Service("public-api")).WithOutput(log.NewOutput(writer).WithFormatter(log.NewHumanReadableFormatter()))
+		serviceLogger.Info("StringableSlice HR test", log.StringableSlice("a-collection", transactions))
+	})
+
+	fmt.Println(stdout)
+
+	Expect(stdout).To(HavePrefix("info"))
+	Expect(stdout).To(ContainSubstring("StringableSlice HR test"))
+	Expect(stdout).To(ContainSubstring("node=node1"))
+	Expect(stdout).To(ContainSubstring("service=public-api"))
+	Expect(stdout).To(ContainSubstring("a-collection=["))
+	Expect(stdout).To(ContainSubstring("{Transaction:{ProtocolVersion:1,"))
+	Expect(stdout).To(ContainSubstring("function=log_test.TestStringableSliceCustomFormat.func1"))
+	Expect(stdout).To(ContainSubstring("source="))
+	Expect(stdout).To(ContainSubstring("log/basic_logger_test.go"))
+
+}
+
+func TestMeter(t *testing.T) {
+	RegisterTestingT(t)
+
+	stdout := captureStdout(func(writer io.Writer) {
+		serviceLogger := log.GetLogger(log.Node("node1"), log.Service("public-api")).WithOutput(log.NewOutput(writer))
+		txId := log.String("txId", "1234567")
+		txFlowLogger := serviceLogger.For(log.String("flow", TransactionFlow))
+		meter := txFlowLogger.Meter("tx-process-time", txId)
+		defer meter.Done()
+
+		time.Sleep(1 * time.Millisecond)
+	})
+
+	fmt.Println(stdout)
+
+	jsonMap := parseOutput(stdout)
+
+	Expect(jsonMap["level"]).To(Equal("metric"))
+	Expect(jsonMap["node"]).To(Equal("node1"))
+	Expect(jsonMap["service"]).To(Equal("public-api"))
+	Expect(jsonMap["function"]).To(Equal("log_test.TestMeter.func1"))
+	Expect(jsonMap["message"]).To(Equal("Metric recorded"))
+	Expect(jsonMap["source"]).NotTo(BeEmpty())
+	Expect(jsonMap["timestamp"]).NotTo(BeNil())
+	Expect(jsonMap["metric"]).To(Equal("public-api-TransactionFlow-tx-process-time"))
+	Expect(jsonMap["txId"]).To(Equal("1234567"))
+	Expect(jsonMap["flow"]).To(Equal(TransactionFlow))
+	Expect(jsonMap["process-time"]).NotTo(BeNil())
+}
+
+func TestCustomLogFormatter(t *testing.T) {
+	RegisterTestingT(t)
+
+	stdout := captureStdout(func(writer io.Writer) {
+		serviceLogger := log.GetLogger(log.Node("node1"), log.Service("public-api")).WithOutput(log.NewOutput(writer).WithFormatter(log.NewHumanReadableFormatter()))
+		serviceLogger.Info("Service initialized", log.Int("some-int-value", 12), log.BlockHeight(primitives.BlockHeight(9999)), log.Bytes("bytes", []byte{2, 3, 99}), log.Stringable("vchainId", primitives.VirtualChainId(123)))
+	})
+
+	fmt.Println(stdout)
+
+	Expect(stdout).To(HavePrefix("info"))
+	Expect(stdout).To(ContainSubstring("Service initialized"))
+	Expect(stdout).To(ContainSubstring("node=node1"))
+	Expect(stdout).To(ContainSubstring("service=public-api"))
+	Expect(stdout).To(ContainSubstring("block-height=270f"))
+	Expect(stdout).To(ContainSubstring("vchainId=7b"))
+	Expect(stdout).To(ContainSubstring("bytes=gDp"))
+	Expect(stdout).To(ContainSubstring("some-int-value=12"))
+	Expect(stdout).To(ContainSubstring("function=log_test.TestCustomLogFormatter.func1"))
+	Expect(stdout).To(ContainSubstring("source="))
+	Expect(stdout).To(ContainSubstring("log/basic_logger_test.go"))
+}
+
+func TestMultipleOutputs(t *testing.T) {
+	RegisterTestingT(t)
+
+	filename := "/tmp/test-multiple-outputs"
+	os.RemoveAll(filename)
+
+	fileOutput, _ := os.Create(filename)
+
+	stdout := captureStdout(func(writer io.Writer) {
+		serviceLogger := log.GetLogger(log.Node("node1"), log.Service("public-api")).WithOutput(log.NewOutput(writer), log.NewOutput(fileOutput))
+		serviceLogger.Info("Service initialized")
+	})
+
+	rawFile, _ := ioutil.ReadFile(filename)
+	fileContents := string(rawFile)
+
+	fmt.Println(fileContents)
+
+	checkOutput(stdout)
+	checkOutput(fileContents)
+}
+
+func TestMultipleOutputsForMemoryViolationByHumanReadable(t *testing.T) {
+	RegisterTestingT(t)
+
+	filename := "/tmp/test-multiple-outputs"
+	os.RemoveAll(filename)
+
+	fileOutput, _ := os.Create(filename)
+
+	Expect(func() {
+		captureStdout(func(writer io.Writer) {
+			serviceLogger := log.GetLogger(log.Node("node1"), log.Service("public-api")).WithOutput(log.NewOutput(writer).WithFormatter(log.NewHumanReadableFormatter()), log.NewOutput(fileOutput))
+			serviceLogger.Info("Service initialized")
+		})
+	}).NotTo(Panic())
+}
+
+func checkOutput(output string) {
+	jsonMap := parseOutput(output)
+
+	Expect(jsonMap["level"]).To(Equal("info"))
+	Expect(jsonMap["node"]).To(Equal("node1"))
+	Expect(jsonMap["service"]).To(Equal("public-api"))
+	Expect(jsonMap["function"]).To(Equal("log_test.TestMultipleOutputs.func1"))
+	Expect(jsonMap["message"]).To(Equal("Service initialized"))
+	Expect(jsonMap["source"]).NotTo(BeEmpty())
+	Expect(jsonMap["timestamp"]).NotTo(BeNil())
+}
