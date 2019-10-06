9c0453e38e15b1a8d746411a5e51f56b31a1ae40
aviram <aviram@orbs.com>
2019-10-06 11:05:39 +0300

    - Adding a default request timeout of 60 seconds for HttpWriter - Treating HTTP 2XX as success, instead of only HTTP 200

--- log/bulk_output.go
+++ log/bulk_output.go
@@ -79,26 +79,34 @@ func NewBulkOutput(writer io.Writer, formatter LogFormatter, bulkSize int) Outpu
 }
 
 type httpWriter struct {
-	url string
+	url     string
+	timeout time.Duration
 }
 
 func (w *httpWriter) Write(p []byte) (n int, err error) {
 	reader := bytes.NewReader(p)
 	size := reader.Len()
-	resp, err := http.Post(w.url, "application/json", reader)
+	client := &http.Client{
+		Timeout: w.timeout,
+	}
+	resp, err := client.Post(w.url, "application/json", reader)
 
 	if err != nil {
 		return 0, errors.Errorf("Failed to send logs: %s", err)
 	}
 
 	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
 		return 0, errors.Errorf("Failed to send logs: %d, %s", resp.StatusCode, err)
 	}
 
 	return size, err
 }
 
-func NewHttpWriter(url string) io.Writer {
-	return &httpWriter{url}
+func NewHttpWriter(url string) *httpWriter {
+	return &httpWriter{url, time.Second * 60}
+}
+
+func NewHttpWriterWithTimeout(url string, timeout time.Duration) *httpWriter {
+	return &httpWriter{url, timeout}
 }
