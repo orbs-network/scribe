a81297f706126d79e171a790ad2af603a3a9692a
amir-arad <amir@orbs.com>
2019-10-31 10:56:47 +0200

    apply lock to all TestOutput API

--- log/test_output_test.go
+++ log/test_output_test.go
@@ -93,6 +93,27 @@ func TestOutputFailsTestEvenAfterTermination(t *testing.T) {
 	require.NoError(t, err)
 }
 
+// designed for race detector
+func TestOutputSynchronizesHasErrorsAndAppendError(t *testing.T) {
+	m := &fakeTLog{}
+	o := NewTestOutput(m, nopFormatter{})
+	ch := make(chan int)
+	go func() {
+		for i := 1; i <= 100; i++ {
+			o.Append("error", "foo")
+		}
+		ch <- 0
+	}()
+	go func() {
+		for i := 1; i <= 100; i++ {
+			o.HasErrors()
+		}
+		ch <- 0
+	}()
+	_ = <-ch
+	_ = <-ch
+}
+
 type fakeTLog struct {
 	mock.Mock
 }
